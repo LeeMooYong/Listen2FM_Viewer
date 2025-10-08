cedc9056f6ad25f66ce3f4e434e40fa79542ffd5
LeeMooYong <needsitem@naver.com>
2025-10-08 19:59:49 +0900

    clear repo.

--- plugins/crypto/sync/priceAxisSync.js
+++
@@ -1,6 +0,0 @@
-// plugins/crypto/sync/priceAxisSync.js
-// NOTE: 구현은 shared 모듈로 승격되었고, crypto 경로에서는 재수출만 합니다.
-// 기존 코드가 default 또는 named import를 모두 사용할 수 있도록 둘 다 재수출합니다.
-
-export { default } from "../../shared/sync/priceAxisSync.js"; // default import 호환
-export * from "../../shared/sync/priceAxisSync.js";          // named import 호환
--- plugins/crypto/sync/timeScaleLinker.js
+++
@@ -1,5 +0,0 @@
-// plugins/crypto/sync/timeScaleLinker.js
-// NOTE: time scale 유틸은 보통 named export를 사용합니다.
-// shared 모듈의 export들을 그대로 재수출합니다.
-
-export * from "../../shared/sync/timeScaleLinker.js";
